4c54453e44ebbbe4e5a5e25fb7b3c175ce216aa1
psiini <ssaini456123@gmail.com>
2026-05-09 11:06:35 -0700

    Create SIERPASCII.f

---
+++ SIERPASCII.f
@@ -0,0 +1,15 @@
+C23456789 ...
+       PROGRAM SIERPASCII
+       INTEGER X, Y, N
+       
+       DO 10 Y=0,20
+        DO 20 X=0,Y
+            IF (IAND(X, Y-X) .EQ. 0) THEN
+                WRITE (*,'(A)', ADVANCE='NO') '@'
+            ELSE
+                WRITE (*,'(A)', ADVANCE='NO') ' '
+            END IF
+20    CONTINUE
+      PRINT *, ' '
+10    CONTINUE
+        END
